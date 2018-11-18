c5dc913cfb4903e64f5893f7da97d213be4fdb74
jnafolayan <jnafolayan@gmail.com>
2018-11-18 10:14:41 +0100

    add sql statements

---
+++ sql/create_admin.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (
+  firstname,
+  lastname,
+  othernames,
+  email,
+  username,
+  is_admin
+) VALUES ('John', 'Afolayan', 'Oluwagbemiga', 'hughj32001@gmail.com', 'jnafolayan', TRUE);
---
+++ sql/createdb.sql
@@ -0,0 +1,25 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  firstname VARCHAR (100) NOT NULL,
+  lastname VARCHAR (100) NOT NULL,
+  othernames VARCHAR (355) NOT NULL,
+  email VARCHAR (355) NOT NULL,
+  username VARCHAR (100) NOT NULL,
+  registered DATE DEFAULT CURRENT_DATE,
+  is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE parcels (
+  id serial PRIMARY KEY,
+  placed_by UUID REFERENCES users (id) ON DELETE CASCADE,
+  weight REAL NOT NULL,
+  weightmetric VARCHAR (5) NOT NULL,
+  sentOn DATE DEFAULT CURRENT_DATE,
+  delivered_on DATE,
+  status VARCHAR (20) NOT NULL,
+  from_loc VARCHAR (355) NOT NULL,
+  to_loc VARCHAR (355) NOT NULL,
+  current_loc VARCHAR (355) NOT NULL
+);
